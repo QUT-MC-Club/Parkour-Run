26f771f19bdc68ab225cde2cbcd3c03da39a6279
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-03-15 23:54:45 -0400

    Update minigame to Minecraft 1.18.2

--- src/main/java/io/github/haykam821/parkourrun/Main.java
+++ src/main/java/io/github/haykam821/parkourrun/Main.java
@@ -3,17 +3,17 @@ package io.github.haykam821.parkourrun;
 import io.github.haykam821.parkourrun.game.ParkourRunConfig;
 import io.github.haykam821.parkourrun.game.phase.ParkourRunWaitingPhase;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import xyz.nucleoid.plasmid.game.GameType;
 
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "parkourrun";
 
 	private static final Identifier ENDING_PLATFORMS_ID = new Identifier(MOD_ID, "ending_platforms");
-	public static final Tag<Block> ENDING_PLATFORMS = TagFactory.BLOCK.create(ENDING_PLATFORMS_ID);
+	public static final TagKey<Block> ENDING_PLATFORMS = TagKey.of(Registry.BLOCK_KEY, ENDING_PLATFORMS_ID);
 
 	private static final Identifier PARKOUR_RUN_ID = new Identifier(MOD_ID, "parkour_run");
 	public static final GameType<ParkourRunConfig> PARKOUR_RUN_TYPE = GameType.register(PARKOUR_RUN_ID, ParkourRunConfig.CODEC, ParkourRunWaitingPhase::open);
